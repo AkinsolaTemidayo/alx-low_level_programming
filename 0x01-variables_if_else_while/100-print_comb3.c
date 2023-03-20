72f32f8563e19a81550dd51e447cb7dbd19f5290
AkinsolaTemidayo <temidayoakinsola100@gmail.com>
2023-03-20 09:01:35 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
 
 /**
- * main - Entry
- * Return: always 0
+ * main - Prints numbers between 00 to 89.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
+int i, e;
 
-	for (i = 0 ; i < 10 ; i++)
-	{
-		putchar(i + '0');
-	}
-	putchar('\n');
-	return (0);
+i = 48;
+e = 48;
+
+while (e < 58)
+{
+i = 48;
+while (i < 58)
+{
+if (e != i && e < i)
+{
+putchar(e);
+putchar(i);
+if (i == 57 && e == 56)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+i++;
+}
+e++;
+}
+putchar('\n');
+return (0);
 }
