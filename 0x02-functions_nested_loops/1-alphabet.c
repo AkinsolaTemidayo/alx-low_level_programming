1be3466814a60d4460d291de691a050ffcb06662
AkinsolaTemidayo <temidayoakinsola100@gmail.com>
2023-03-21 13:08:08 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Enyry point
+ * Retirn: Always 0
+ */
+
+void print_alphabet(void)
+
+{
+	char c;
+
+	for ( c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
